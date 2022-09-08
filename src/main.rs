c56f5bfdbe33d6028923686e8d274d5d38852aa2
KENZ <KENZ.gelsoft@gmail.com>
2022-09-09 01:21:13 +0900

    タイトルを変更、使わないコードを削除

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,16 @@ use wx::methods::*;
 fn main() {
     wx::App::run(|_| {
         let frame = wx::Frame::builder(wx::Window::none())
-            .title("Hello, 世界")
+            .title("カニツメエディタ")
             .build();
         let button = wx::Button::builder(Some(&frame)).label("Greet").build();
-        let i = 3;
-        println!("i={}", i);
         let weak_button = button.to_weak_ref();
         button.bind(wx::RustEvent::Button, move |_: &wx::CommandEvent| {
             if let Some(button) = weak_button.get() {
-                println!("i={}", i);
                 button.set_label("clicked");
                 println!("s={}", button.get_label())
             }
         });
-        frame.centre(wx::BOTH);
         frame.show(true);
     });
 }
