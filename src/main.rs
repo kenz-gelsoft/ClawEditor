f5913bff043de752dec7b9b0ee65df88c2998d26
KENZ <KENZ.gelsoft@gmail.com>
2022-09-09 01:18:37 +0900

    Copy doc's sample as base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![windows_subsystem = "windows"]
+
+use wx;
+use wx::methods::*;
+
 fn main() {
-    println!("Hello, world!");
+    wx::App::run(|_| {
+        let frame = wx::Frame::builder(wx::Window::none())
+            .title("Hello, 世界")
+            .build();
+        let button = wx::Button::builder(Some(&frame)).label("Greet").build();
+        let i = 3;
+        println!("i={}", i);
+        let weak_button = button.to_weak_ref();
+        button.bind(wx::RustEvent::Button, move |_: &wx::CommandEvent| {
+            if let Some(button) = weak_button.get() {
+                println!("i={}", i);
+                button.set_label("clicked");
+                println!("s={}", button.get_label())
+            }
+        });
+        frame.centre(wx::BOTH);
+        frame.show(true);
+    });
 }
