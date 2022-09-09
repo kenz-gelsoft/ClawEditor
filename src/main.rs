94a6300afdb17e0359a572ec177ad7a60073b9f8
KENZ <KENZ.gelsoft@gmail.com>
2022-09-10 00:46:51 +0900

    メニュー処理の枠組みを実装

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@ use std::os::raw::c_int;
 use wx;
 use wx::methods::*;
 
-enum File {
-    New = 100,
-    Open,
-    Close,
-    Save,
+#[derive(Clone, Copy)]
+enum Command {
+    FileNew = wx::ID_HIGHEST as isize,
+    FileOpen,
+    FileClose,
+    FileSave,
 }
-impl From<File> for c_int {
-    fn from(w: File) -> Self {
+impl Command {
+    fn from(v: c_int) -> Option<Self> {
+        use Command::*;
+        for e in [FileNew, FileOpen, FileClose, FileSave] {
+            if v == e.into() {
+                return Some(e);
+            }
+        }
+        return None;
+    }
+}
+impl From<Command> for c_int {
+    fn from(w: Command) -> Self {
         w as c_int
     }
 }
@@ -23,17 +35,31 @@ fn main() {
             .build();
         let menu_bar = wx::MenuBar::new(0);
         let file_menu = wx::Menu::new()
-            .item(File::New, "新規\tCtrl-N")
-            .item(File::Open, "開く\tCtrl-O")
+            .item(Command::FileNew, "新規\tCtrl-N")
+            .item(Command::FileOpen, "開く\tCtrl-O")
             .separator()
-            .item(File::Close, "閉じる\tCtrl-W")
-            .item(File::Save, "保存\tCtrl-S");
+            .item(Command::FileClose, "閉じる\tCtrl-W")
+            .item(Command::FileSave, "保存\tCtrl-S");
 
         menu_bar.append(Some(&file_menu), "ファイル");
         frame.set_menu_bar(Some(&menu_bar));
         let _textbox = wx::TextCtrl::builder(Some(&frame))
             .style(wx::TE_MULTILINE.into())
             .build();
+        frame.bind(wx::RustEvent::Menu, |event: &wx::CommandEvent| {
+            if let Some(command) = Command::from(event.get_id()) {
+                handle_command(&command);
+            }
+        });
         frame.show(true);
     });
+}
+
+fn handle_command(command: &Command) {
+    match command {
+        Command::FileNew => todo!(),
+        Command::FileOpen => todo!(),
+        Command::FileClose => todo!(),
+        Command::FileSave => todo!(),
+    }
 }
