b5e581c82d8698e5336fc7bcaad6e16acda236c0
KENZ <KENZ.gelsoft@gmail.com>
2022-09-10 11:20:28 +0900

    メモ帳よりのメニュー構成に、保存ダイアログを開く

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use wx::methods::*;
 enum Command {
     FileNew = wx::ID_HIGHEST as isize,
     FileOpen,
-    FileClose,
     FileSave,
+    FileSaveAs,
+    FileClose,
 }
 impl Command {
     fn from(v: c_int) -> Option<Self> {
         use Command::*;
-        for e in [FileNew, FileOpen, FileClose, FileSave] {
+        for e in [FileNew, FileOpen, FileSave, FileSaveAs, FileClose] {
             if v == e.into() {
                 return Some(e);
             }
@@ -53,13 +54,14 @@ fn main() {
 fn build_menu(frame: &wx::Frame) {
     let menu_bar = wx::MenuBar::new(0);
     let file_menu = wx::Menu::new()
-        .item(Command::FileNew, "新規\tCtrl-N")
-        .item(Command::FileOpen, "開く\tCtrl-O")
+        .item(Command::FileNew, "新規(&N)\tCtrl-N")
+        .item(Command::FileOpen, "開く(&W)\tCtrl-O")
+        .item(Command::FileSave, "保存(&S)\tCtrl-S")
+        .item(Command::FileSaveAs, "名前を付けて保存(&A)\tCtrl-Shift-S")
         .separator()
-        .item(Command::FileClose, "閉じる\tCtrl-W")
-        .item(Command::FileSave, "保存\tCtrl-S");
+        .item(Command::FileClose, "終了(&X)\tCtrl-W");
 
-    menu_bar.append(Some(&file_menu), "ファイル");
+    menu_bar.append(Some(&file_menu), "ファイル(&F)");
     frame.set_menu_bar(Some(&menu_bar));
 }
 
@@ -69,8 +71,13 @@ fn handle_command(frame: &Frame, command: &Command) {
         Command::FileOpen => {
             open_file(frame);
         }
-        Command::FileClose => todo!(),
         Command::FileSave => todo!(),
+        Command::FileSaveAs => {
+            save_as(frame);
+        }
+        Command::FileClose => {
+            frame.close(false);
+        }
     }
 }
 
@@ -91,5 +98,25 @@ fn open_file(frame: &Frame) {
         // TODO: open
         let path = file_dialog.get_path();
         println!("open: {}", path);
+    }
+}
+
+fn save_as(frame: &Frame) {
+    // TODO: Add Builder for wx::FileDialog
+    let file_dialog = wx::FileDialog::new(
+        Some(frame),
+        "",
+        "",
+        "",
+        "*.*",
+        wx::FC_SAVE.into(),
+        &wx::Point::default(),
+        &wx::Size::default(),
+        "",
+    );
+    if wx::ID_OK == file_dialog.show_modal() {
+        // TODO: open
+        let path = file_dialog.get_path();
+        println!("save as: {}", path);
     }
 }
