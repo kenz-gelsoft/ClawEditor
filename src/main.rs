745cd9519a7abbd70a304be33118a84056e3750d
KENZ <KENZ.gelsoft@gmail.com>
2022-09-11 10:24:54 +0900

    EditorFrame structを導入

--- src/main.rs
+++ src/main.rs
@@ -86,10 +86,19 @@ type TextCtrl = wx::TextCtrlIsOwned<false>;
 
 fn main() {
     wx::App::run(|_| {
+        let frame = EditorFrame::new();
+        frame.base.show(true);
+    });
+}
+
+struct EditorFrame {
+    base: wx::Frame,
+}
+impl EditorFrame {
+    fn new() -> Self {
         let frame = wx::Frame::builder(wx::Window::none())
             .title("カニツメエディタ")
             .build();
-        build_menu(&frame);
         let textbox = wx::TextCtrl::builder(Some(&frame))
             .style(wx::TE_MULTILINE.into())
             .build();
@@ -106,56 +115,58 @@ fn main() {
                 }
             }
         });
-        frame.show(true);
-    });
-}
+        let frame = EditorFrame { base: frame };
+        frame.build_menu();
+        frame
+    }
 
-fn build_menu(frame: &wx::Frame) {
-    let menu_bar = wx::MenuBar::new(0);
+    fn build_menu(&self) {
+        let menu_bar = wx::MenuBar::new(0);
 
-    let file_menu = wx::Menu::new()
-        .item(Command::FileNew, "新規(&N)\tCtrl-N")
-        .item(Command::FileNewWindow, "新しいウィンドウ(&W)\tCtrl-Shift-N")
-        .item(Command::FileOpen, "開く(&W)…\tCtrl-O")
-        .item(Command::FileSave, "保存(&S)\tCtrl-S")
-        .item(Command::FileSaveAs, "名前を付けて保存(&A)…\tCtrl-Shift-S")
-        .separator()
-        .item(Command::FileClose, "終了(&X)\tCtrl-W");
-    menu_bar.append(Some(&file_menu), "ファイル(&F)");
+        let file_menu = wx::Menu::new()
+            .item(Command::FileNew, "新規(&N)\tCtrl-N")
+            .item(Command::FileNewWindow, "新しいウィンドウ(&W)\tCtrl-Shift-N")
+            .item(Command::FileOpen, "開く(&W)…\tCtrl-O")
+            .item(Command::FileSave, "保存(&S)\tCtrl-S")
+            .item(Command::FileSaveAs, "名前を付けて保存(&A)…\tCtrl-Shift-S")
+            .separator()
+            .item(Command::FileClose, "終了(&X)\tCtrl-W");
+        menu_bar.append(Some(&file_menu), "ファイル(&F)");
 
-    let edit_menu = wx::Menu::new()
-        .item(wx::ID_UNDO, "元に戻す(&U)\tCtrl-Z")
-        .separator()
-        .item(wx::ID_CUT, "切り取り(&T)\tCtrl-X")
-        .item(wx::ID_COPY, "コピー(&C)\tCtrl-C")
-        .item(wx::ID_PASTE, "貼り付け(&P)\tCtrl-V")
-        .item(Command::EditDelete, "削除(&L)\tDel")
-        .separator()
-        .item(Command::EditFind, "検索(&F)…\tCtrl-F")
-        .item(Command::EditFindNext, "次を検索(&N)\tF3")
-        .item(Command::EditFindPrevious, "前を検索(&V)\tShift-F3")
-        .item(Command::EditReplace, "置換(&R)…\tCtrl-H")
-        .item(Command::EditGo, "行へ移動(&G)…\tCtrl-G")
-        .separator()
-        .item(wx::ID_SELECTALL, "すべて選択(&A)\tCtrl-A")
-        .item(Command::EditDate, "日付と時刻(&D)\tF5");
-    menu_bar.append(Some(&edit_menu), "編集(&E)");
+        let edit_menu = wx::Menu::new()
+            .item(wx::ID_UNDO, "元に戻す(&U)\tCtrl-Z")
+            .separator()
+            .item(wx::ID_CUT, "切り取り(&T)\tCtrl-X")
+            .item(wx::ID_COPY, "コピー(&C)\tCtrl-C")
+            .item(wx::ID_PASTE, "貼り付け(&P)\tCtrl-V")
+            .item(Command::EditDelete, "削除(&L)\tDel")
+            .separator()
+            .item(Command::EditFind, "検索(&F)…\tCtrl-F")
+            .item(Command::EditFindNext, "次を検索(&N)\tF3")
+            .item(Command::EditFindPrevious, "前を検索(&V)\tShift-F3")
+            .item(Command::EditReplace, "置換(&R)…\tCtrl-H")
+            .item(Command::EditGo, "行へ移動(&G)…\tCtrl-G")
+            .separator()
+            .item(wx::ID_SELECTALL, "すべて選択(&A)\tCtrl-A")
+            .item(Command::EditDate, "日付と時刻(&D)\tF5");
+        menu_bar.append(Some(&edit_menu), "編集(&E)");
 
-    let format_menu = wx::Menu::new()
-        .check_item(Command::FormatWordWrap, "右端で折り返す(&W)")
-        .item(Command::FormatFont, "フォント(&O)…");
-    menu_bar.append(Some(&format_menu), "書式(&O)");
+        let format_menu = wx::Menu::new()
+            .check_item(Command::FormatWordWrap, "右端で折り返す(&W)")
+            .item(Command::FormatFont, "フォント(&O)…");
+        menu_bar.append(Some(&format_menu), "書式(&O)");
 
-    let view_menu = wx::Menu::new().check_item(Command::ViewStatusBar, "ステータスバー(&S)");
-    menu_bar.append(Some(&view_menu), "表示(&V)");
+        let view_menu = wx::Menu::new().check_item(Command::ViewStatusBar, "ステータスバー(&S)");
+        menu_bar.append(Some(&view_menu), "表示(&V)");
 
-    let help_menu = wx::Menu::new()
-        .item(Command::Help, "ヘルプの表示(&H)")
-        .separator()
-        .item(Command::HelpAbout, "バージョン情報(&A)");
-    menu_bar.append(Some(&help_menu), "ヘルプ(&H)");
+        let help_menu = wx::Menu::new()
+            .item(Command::Help, "ヘルプの表示(&H)")
+            .separator()
+            .item(Command::HelpAbout, "バージョン情報(&A)");
+        menu_bar.append(Some(&help_menu), "ヘルプ(&H)");
 
-    frame.set_menu_bar(Some(&menu_bar));
+        self.base.set_menu_bar(Some(&menu_bar));
+    }
 }
 
 fn handle_command(frame: &Frame, textbox: &TextCtrl, command: &Command) {
