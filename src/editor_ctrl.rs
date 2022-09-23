80749a05550244286048c21ccade545de6d65041
KENZ <KENZ.gelsoft@gmail.com>
2022-09-23 11:06:41 +0900

    変更フラグにともなう制御の振る舞いテスト
    * 変更フラグ管理処理は後で抽出する

--- src/editor_ctrl.rs
+++ src/editor_ctrl.rs
@@ -12,6 +12,131 @@ pub enum DocumentEvent {
     TextModified,
 }
 
+// 保存されていない変更を管理する
+trait UnsavedChange {
+    fn new_file();
+}
+
+trait UnsavedChangeUI {
+    fn get_path_to_save<CB: FnMut(Option<String>)>(&mut self, callback: CB);
+}
+
+// 後で実際の Document を使うようにする
+trait DummyDoc {
+    // 変更フラグ
+    fn is_modified(&self) -> bool;
+    fn set_modified(&mut self, modified: bool);
+
+    fn save_to(&mut self, path: String) -> bool;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // TODO: mockall を試す
+    struct MockDoc {
+        modified: bool,
+        save_will_fail: bool,
+    }
+    impl MockDoc {
+        fn new() -> Self {
+            Self {
+                modified: false,
+                save_will_fail: false,
+            }
+        }
+    }
+    impl DummyDoc for MockDoc {
+        fn is_modified(&self) -> bool {
+            self.modified
+        }
+        fn set_modified(&mut self, modified: bool) {
+            self.modified = modified
+        }
+
+        fn save_to(&mut self, _: String) -> bool {
+            if self.save_will_fail {
+                return false;
+            }
+            self.modified = false;
+            true
+        }
+    }
+
+    struct MockSaveDialog {
+        will_be_cancelled: bool,
+    }
+    impl MockSaveDialog {
+        fn new() -> Self {
+            Self {
+                will_be_cancelled: false,
+            }
+        }
+    }
+    impl UnsavedChangeUI for MockSaveDialog {
+        fn get_path_to_save<CB: FnMut(Option<String>)>(&mut self, mut callback: CB) {
+            if self.will_be_cancelled {
+                callback(None);
+                return;
+            }
+            callback(Some("path/to/save".to_owned()))
+        }
+    }
+
+    #[test]
+    fn modified_doc_will_be_unmodified_after_save() {
+        // Given: ドキュメントの変更フラグが立っている状態から
+        let mut doc = MockDoc::new();
+        doc.set_modified(true);
+        assert!(doc.is_modified());
+
+        let mut save_dlg = MockSaveDialog::new();
+        save_dlg.get_path_to_save(move |path| {
+            assert_ne!(path, None);
+            // When: 保存に成功したら
+            assert!(doc.save_to(path.unwrap()));
+            // Then: 変更フラグが倒れている
+            assert!(!doc.is_modified());
+        });
+    }
+
+    #[test]
+    fn modified_doc_keeps_modified_after_save_cancelled() {
+        // Given: ドキュメントの変更フラグが立っている状態から
+        let mut doc = MockDoc::new();
+        doc.set_modified(true);
+        assert!(doc.is_modified());
+
+        let mut save_dlg = MockSaveDialog::new();
+        // When: 保存がキャンセルされたら
+        save_dlg.will_be_cancelled = true;
+        save_dlg.get_path_to_save(move |path| {
+            assert_eq!(path, None);
+            // Then: 変更フラグは立ったまま
+            assert!(doc.is_modified());
+        });
+    }
+
+    #[test]
+    fn modified_doc_keeps_modified_after_save_failed() {
+        // Given: ドキュメントの変更フラグが立っている状態から
+        let mut doc = MockDoc::new();
+        doc.set_modified(true);
+        assert!(doc.is_modified());
+
+        let mut save_dlg = MockSaveDialog::new();
+        save_dlg.get_path_to_save(move |path| {
+            assert_ne!(path, None);
+            // When: 保存に失敗したら
+            doc.save_will_fail = true;
+            assert!(!doc.save_to(path.unwrap()));
+            // Then: 変更フラグは立ったまま
+            assert!(doc.is_modified());
+        });
+    }
+}
+
 pub trait Document {
     fn events(&self) -> Rc<RefCell<Subject<DocumentEvent>>>;
     fn clear(&self);
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(not(test), windows_subsystem = "windows")]
 
 use wx;
 
