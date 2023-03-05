efacf245ab8211c51593331da315d526a73f0c16
KENZ <KENZ.gelsoft@gmail.com>
2023-03-05 17:54:55 +0900

    #10 wx.rc (を include する)リソースファイルをリンク(2)
    %wxwin% 環境変数が定義されている場合のみ実行するようにした

--- build.rs
+++ build.rs
@@ -21,11 +21,13 @@ fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> 
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut wxwin = PathBuf::from(env::var("wxwin")?);
-    wxwin.push(r"include\wx\msw");
-    let mut out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    out_dir.push(r"wx\msw");
-    copy_dir_all(wxwin, out_dir).ok();
-    embed_resource::compile("wxRust.rc");
+    if let Ok(wxwin) = env::var("wxwin") {
+        let mut wx_rc_dir = PathBuf::from(wxwin);
+        wx_rc_dir.push(r"include\wx\msw");
+        let mut out_dir = PathBuf::from(env::var("OUT_DIR")?);
+        out_dir.push(r"wx\msw");
+        copy_dir_all(wx_rc_dir, out_dir).ok();
+        embed_resource::compile("wxRust.rc");
+    }
     Ok(())
 }
